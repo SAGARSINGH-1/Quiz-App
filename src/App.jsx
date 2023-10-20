b56cc7b73f874989c5de0bfee72ccb12398c5620
Sagar Singh <sagar.01ko@gmail.com>
2023-10-20 23:55:42 +0530

    Fixes File Location

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Layout from "./Components/Layout/Layout"
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-     <h1 className='text-5xl font-semibold text-center text-red-500'>Hello Sagar</h1>
+    <div className="flex justify-center h-[100vh]">
+      <Layout />
     </div>
   )
 }
