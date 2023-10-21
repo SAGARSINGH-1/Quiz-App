6dce1c7ea29118bd16023577152efc40611f8e14
Sagar Singh <sagar.01ko@gmail.com>
2023-10-21 14:40:53 +0530

    updted "sections" and "component"

--- src/Components/Home/01-Home.jsx
+++ src/Components/Home/01-Home.jsx
@@ -8,7 +8,23 @@ export default function Home() {
   return (
     <div className='overflow-x-hidden overflow-y-hidden'>
       <Hero />
-      <About/>
+
+      {/* About section */}
+      <div className='flex justify-center p-5 gap-10 overflow-hidden'>
+        <div className='img'>
+          <img src="./assets/About.gif" alt="about" height='450px' width='450px' />
+        </div>
+        <div className='content p-5  mt-[8vh] w-[30vw] py-10' >
+          <div className='text'>
+            <h4 className='text-2xl text-indigo-500 font-semibold ml-5'> Lifelong Learning</h4>
+            <h1 className='text-4xl font-semibold m-3'>Nurturing Curiosity Through Quizzes</h1>
+            <p className='text-lg text-gray-500 m-5'>At our platform, we believe in the power of quizzes to enhance your learning adventure.</p>
+          </div>
+          <div>
+            <button className='ml-10'>Know More</button>
+          </div>
+        </div>
+      </div>
       <Selection />
       <Question />
     </div>
--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -1,1 +1,7 @@
-export default function Contact() {}
+export default function Contact() {
+    return(
+        <div className=" flex justify-center p-3 overflow-hidden">
+            This is contact
+        </div>
+    )
+}
