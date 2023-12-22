36767083fb0fa5a5942a0af427f5f0271741874c
Manendra Verma <manuvma03@gmail.com>
2023-12-22 19:21:57 +0530

    Add message in contact component

--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return(
-        <div className=" flex justify-center p-3 overflow-hidden">
-            This is contact
+        <div className=" flex justify-center items-center overflow-hidden h-[80vh]">
+            <h1 className="text-xl text-gray-400 font-semibold">Sorry for the inconvenience, This page is Under Development!</h1>
         </div>
     )
 }
